9b753923737e0e33623c1e26fff05baf2bcd8aec
Aaron Yih <aaron.yih@ucla.edu>
2015-12-22 21:02:28 -0800

    poop

---
+++ models/Project.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var projectSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  contentPiece: String,
+  coverPhotoURL: String,
+  pictureURLS: [String],
+  author_fullName: String,
+  author_id: mongoose.Schema.Types.ObjectId,
+  dateCreated: {type: Date, default: Date.now},
+
+});
+
+module.exports = Project = mongoose.model('Project', projectSchema);
